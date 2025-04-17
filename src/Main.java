f89334f7848d28b53876f6b77aaa6f2ddd59c2bc
Gaoussou Doucoure <gaousd4@gmail.com>
2025-04-16 23:49:10 -0700

    update

--- src/Main.java
+++ src/Main.java
@@ -9,16 +9,34 @@ public class Main {
         System.out.print("Please enter your first name: ");
         String firstName = sc.nextLine();
         firstName = firstName.trim();
-        System.out.print("Please enter your middle name, if no middle name then enter this (none): ");
+        System.out.print("Please enter your middle name, if no middle name then write the word none: ");
         String middleName = sc.nextLine();
         middleName = middleName.trim();
         System.out.print("Please enter your last name: ");
         String lastName = sc.nextLine();
         lastName = lastName.trim();
 
-        String fullName = firstName + (middleName.equals("(none)") ? "" : " " + middleName) + " " + lastName ;
+        String fullName = firstName + (middleName.equals("none") ? "" : " " + middleName) + " " + lastName ;
         fullName = fullName.trim();
 
-        System.out.println(fullName);
+        System.out.printf("Full name is: %s",fullName);
+
+
+//USING STRINGBUILDER
+/*
+// Adding all names together
+        StringBuilder fullName = new StringBuilder();
+        fullName.append(firstName);
+
+        if (!middleName.equals("none")) {
+            fullName.append(" ").append(middleName);
+        }
+
+        fullName.append(" ").append(lastName);
+
+        System.out.printf("Full Name: %s" + fullName);
+
+ */
+
     }
 }
